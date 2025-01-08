9f71bc97ea07ca6286ca289844b398604f7ee59e
timmyvm <tim.mota21@gmail.com>
2025-01-08 20:46:09 +1100

    first commit

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdtmVGLuPHwMBQYNjAr_eDVUC80Qj2k1E",
+  authDomain: "hollywoodai.firebaseapp.com",
+  projectId: "hollywoodai",
+  storageBucket: "hollywoodai.firebasestorage.app",
+  messagingSenderId: "991407263217",
+  appId: "1:991407263217:web:0f6cbbed27f3b2bdfdc850"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
