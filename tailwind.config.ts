cecffe73c547e632f5e2454c31db039c2485d127
timmyvm <tim.mota21@gmail.com>
2025-01-26 06:46:06 +1100

    Screenbreakpoint bug fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,13 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      'xs': '30rem',
+   screens: {
+      'xs': '30rem', // Custom small screen
+      'sm': '40rem', // 640px
+      'md': '48rem', // 768px
+      'lg': '64rem', // 1024px
+      'xl': '80rem', // 1280px
+      '2xl': '96rem', // 1536px
     },
     extend: {
       colors: {
